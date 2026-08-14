b42f84a2e8b4f7a314b62b95ed37789cdfc39750
RujiRum <jdphillips119@gmail.com>
2026-08-13 21:53:20 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
-const CACHE='fayette-atlas-v1';
+const CACHE='fayette-atlas-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+
+self.addEventListener('install',e=>e.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())
+));
+
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys()
+    .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    .then(()=>self.clients.claim())
+));
+
 self.addEventListener('fetch',e=>{
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
     if(e.request.method==='GET' && new URL(e.request.url).origin===location.origin){
